cccbc3d065a048fb4c384cb381f29ca4195ccd46
sparah27 <sarahspahic28@gmail.com>
2023-10-10 20:16:22 +0200

    Spring Initializr
    Creating project with dependencies

---
+++ src/main/java/com/Application/SportsResult/SportsResultApplication.java
@@ -0,0 +1,13 @@
+package com.Application.SportsResult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsResultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsResultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Application/SportsResult/SportsResultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Application.SportsResult;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsResultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
